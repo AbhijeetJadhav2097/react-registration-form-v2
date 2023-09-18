4d71cd3a2660c1d24db5083770ab87c8bbd86cdd
Abhijeet Jadhav <abhijeetjadhav2097@gmail.com>
2023-09-18 18:20:45 +0530

    form-validation-v2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import RegistrationForm from './component/RegistrationForm/index';
+function App() {
+  return (
+    <div className="App">
+        < RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
